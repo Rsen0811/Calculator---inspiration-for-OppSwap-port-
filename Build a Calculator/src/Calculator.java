b8c29469406b1132dad33268c3b2d133172315f2
s-rasengupta <s-rasengupta@487M113748.lwsd.k12wa.org>
2022-09-14 11:43:33 -0700

    test day 1

--- Build a Calculator/src/Calculator.java
+++ Build a Calculator/src/Calculator.java
@@ -4,9 +4,32 @@
  * You should add additional classes as appropriate to support good modularity and reduce redundancy.
  *
  */
-public class Calculator {
-	public static void main() {
-		
-	}
+//import java.utils.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
 
+public class Calculator extends JFrame implements ActionListener {
+	static JFrame frame;
+	private String sTextFeild = "";
+	
+	public static void main() {
+		frame = new JFrame("calculator");
+		
+		Calculator c = new Calculator();		
+        
+        JPanel panel = new JPanel();     
+        frame.add(panel);
+        
+        frame.setSize(200, 220);
+        frame.show();
+	}
+	
+	public Calculator() {
+		System.out.println("Calculator Running");
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.print("hit");
+	}
 }
