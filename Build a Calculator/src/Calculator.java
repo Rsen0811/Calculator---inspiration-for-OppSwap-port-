f7f321b3e146bf143e9634ca3d4d0c06d05f5109
s-rasengupta <s-rasengupta@487M113748.lwsd.k12wa.org>
2022-09-15 13:25:25 -0700

    saving some crazy progress

--- Build a Calculator/src/Calculator.java
+++ Build a Calculator/src/Calculator.java
@@ -9,20 +9,56 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Calculator extends JFrame implements ActionListener {
-	static JFrame frame;
-	private String sTextFeild = "";
+	class Ear implements KeyListener {
+		
+		@Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println(e);
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+        	System.out.println(e);
+        }
+        @Override
+        public void keyTyped(KeyEvent e) {
+        	System.out.println(e);
+        }
+	}
 	
-	public static void main() {
+	static JFrame frame;
+	
+	private String sTextFeild = "";
+	static JTextField o;
+	static JTextField i;
+	
+	public static void main(String[] args) {
 		frame = new JFrame("calculator");
 		
-		Calculator c = new Calculator();		
+		Calculator c = new Calculator();
+		
+		o = new JTextField("MEMES TEST", 18);
+		o.setPreferredSize(new Dimension(250, 100));
+		o.setHorizontalAlignment(JTextField.RIGHT);
+		o.setFont(new Font("Monospace", Font.BOLD, 30));
+		o.setEditable(false);
+		
+		i = new JTextField("HIGH TEST", 40);		
+		i.setPreferredSize(new Dimension(250, 30));		
+		i.setHorizontalAlignment(JTextField.RIGHT);		
+		i.setFont(new Font("Monospace", Font.BOLD, 15));		
+		i.setEditable(false);
+		
+        JPanel panel = new JPanel();
+        panel.add(i);
+        panel.add(o);
         
-        JPanel panel = new JPanel();     
         frame.add(panel);
         
-        frame.setSize(200, 220);
+        frame.setSize(500, 700);
         frame.show();
+        
 	}
 	
 	public Calculator() {
