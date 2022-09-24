cccafd0b5fb753ccc110e9faaceeeda4387a2ec5
s-rasengupta <s-rasengupta@487M113748.lwsd.k12wa.org>
2022-09-23 21:55:40 -0700

    Actually made the calculator a standalone class, rather than it being a code monster inside main

--- Build a Calculator/src/Calculator.java
+++ Build a Calculator/src/Calculator.java
@@ -15,148 +15,142 @@ import java.awt.*;
 public class Calculator extends JFrame implements ActionListener {	
 	static JFrame frame;
 	
-	private static ArrayList<String> history;
-	private static int historyIndex;
+	private ArrayList<String> history;
+	private int historyIndex;
 	
 	private static String otext = "";
-	static JTextField o;
-	static JTextField i;
+	private JTextField o;
+	private JTextField i;
 	
-	public static void main(String[] args) {
-		KeyListener listener = new KeyListener() {
-            
-            @Override
-            public void keyPressed(KeyEvent event) {
-            	char k = event.getKeyChar();
-            	if (event.getKeyCode() == 38) {
-            		scrollHistory(true);
-            	} else if (event.getKeyCode() == 40) {
-            		scrollHistory(false);
-            	}
-
-            	otext = append(otext, k);
-
-            	o.setText(otext.replace('~', '-'));
-                event.consume();
-            }
- 
-            @Override
-            public void keyReleased(KeyEvent event) {
- 
-            } 
- 
-            @Override
-            public void keyTyped(KeyEvent event) {
-                
-            }
-		};
-		
-		frame = new JFrame("calculator");
-		
-		Calculator c = new Calculator();
-		
-		o = new JTextField("MEMES TEST", 18);
-		o.setPreferredSize(new Dimension(250, 100));
-		o.setHorizontalAlignment(JTextField.RIGHT);
-		o.setFont(new Font("Monospace", Font.BOLD, 30));
-		o.setEditable(false);
 	
-		
-		i = new JTextField("HIGH TEST", 40);		
-		i.setPreferredSize(new Dimension(250, 30));		
-		i.setHorizontalAlignment(JTextField.RIGHT);		
-		i.setFont(new Font("Monospace", Font.BOLD, 15));		
-		i.setEditable(false);
-		i.addKeyListener(listener);
-		
-		Map<String, JButton> buttons = new HashMap<String, JButton>();		
-		String[] btnNames = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-								"+", "-", "*", "/", "=", "C", ".", "del", "(-)", "rcl", "▲", "▼", "(", ")", "^"};
-		for (String name : btnNames) {
-			JButton t = new JButton(name);
-			t.setFocusable(false);
-			t.setFont(new Font("Arial", Font.BOLD, 40));
-			t.setPreferredSize(new Dimension(110,70));
-			t.setBackground(new Color((int) (Math.random() * 156)+100, (int) (Math.random() * 206)+50, (int) (Math.random() * 226)+30));
-			
-			if (name == "▲" || name == "▼") {
-				t.setPreferredSize(new Dimension(110,35));
-				t.setFont(new Font("Arial", Font.BOLD, 28));
-			}		
-			
-			t.addActionListener(c);
-			buttons.put(name, t);
-		}
-		
-        JPanel feilds = new JPanel();
-        feilds.setLayout(new BoxLayout(feilds, BoxLayout.Y_AXIS));
-        feilds.add(i);
-        feilds.add(o);
-        
-        JPanel function = new JPanel();
-        
-        JPanel scroll = new JPanel();
-        scroll.setLayout(new GridLayout(2,1));
-        scroll.add(buttons.get("▲"));
-        scroll.add(buttons.get("▼"));
-        
-        function.add(buttons.get("C"));
-        function.add(buttons.get("del"));
-        function.add(buttons.get("rcl"));
-        function.add(scroll);
-        
-        JPanel layer1 = new JPanel(); 
-        
-        layer1.add(buttons.get("("));
-        layer1.add(buttons.get(")"));
-        layer1.add(buttons.get("^"));
-        layer1.add(buttons.get("="));
-        
-        JPanel layer2 = new JPanel();      
-        layer2.add(buttons.get("7"));        
-        layer2.add(buttons.get("8"));
-        layer2.add(buttons.get("9"));
-        layer2.add(buttons.get("/"));
+	KeyListener listener = new KeyListener() {           
+		@Override
+        public void keyPressed(KeyEvent event) {
+        	char k = event.getKeyChar();
+        	if (event.getKeyCode() == 38) {
+        		scrollHistory(true);
+        	} else if (event.getKeyCode() == 40) {
+        		scrollHistory(false);
+        	}
 
-        JPanel layer3 = new JPanel();      
-        layer3.add(buttons.get("4"));        
-        layer3.add(buttons.get("5"));
-        layer3.add(buttons.get("6"));
-        layer3.add(buttons.get("*"));
+        	otext = append(otext, k);
+
+        	o.setText(otext.replace('~', '-'));
+            event.consume();
+        }
+ 
+        @Override
+        public void keyReleased(KeyEvent event) {
+        	return;
+        } 
+ 
+        @Override
+        public void keyTyped(KeyEvent event) {
+             return;   
+        }
+	};{
+		
+	frame = new JFrame("calculator");
+	
+	o = new JTextField("MEMES TEST", 18);
+	o.setPreferredSize(new Dimension(250, 100));
+	o.setHorizontalAlignment(JTextField.RIGHT);
+	o.setFont(new Font("Monospace", Font.BOLD, 30));
+	o.setEditable(false);
+
+	
+	i = new JTextField("HIGH TEST", 40);		
+	i.setPreferredSize(new Dimension(250, 30));		
+	i.setHorizontalAlignment(JTextField.RIGHT);		
+	i.setFont(new Font("Monospace", Font.BOLD, 15));		
+	i.setEditable(false);
+	i.addKeyListener(listener);
+	
+	Map<String, JButton> buttons = new HashMap<String, JButton>();		
+	String[] btnNames = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+							"+", "-", "*", "/", "=", "C", ".", "del", "(-)", "rcl", "▲", "▼", "(", ")", "^"};
+	for (String name : btnNames) {
+		JButton t = new JButton(name);
+		t.setFocusable(false);
+		t.setFont(new Font("Arial", Font.BOLD, 40));
+		t.setPreferredSize(new Dimension(110,70));
+		t.setBackground(new Color((int) (Math.random() * 156)+100, (int) (Math.random() * 206)+50, (int) (Math.random() * 226)+30));
+		
+		if (name == "▲" || name == "▼") {
+			t.setPreferredSize(new Dimension(110,35));
+			t.setFont(new Font("Arial", Font.BOLD, 28));
+		}	
+		
+		
+		t.addActionListener(this);
+		buttons.put(name, t);
+	}
+		
+    JPanel feilds = new JPanel();
+    feilds.setLayout(new BoxLayout(feilds, BoxLayout.Y_AXIS));
+    feilds.add(i);
+    feilds.add(o);
+    
+    JPanel function = new JPanel();
+    
+    JPanel scroll = new JPanel();
+    scroll.setLayout(new GridLayout(2,1));
+    scroll.add(buttons.get("▲"));
+    scroll.add(buttons.get("▼"));
+    
+    function.add(buttons.get("C"));
+    function.add(buttons.get("del"));
+    function.add(buttons.get("rcl"));
+    function.add(scroll);
+    
+    JPanel layer1 = new JPanel(); 
+    
+    layer1.add(buttons.get("("));
+    layer1.add(buttons.get(")"));
+    layer1.add(buttons.get("^"));
+    layer1.add(buttons.get("="));
+    
+    JPanel layer2 = new JPanel();      
+    layer2.add(buttons.get("7"));        
+    layer2.add(buttons.get("8"));
+    layer2.add(buttons.get("9"));
+    layer2.add(buttons.get("/"));
+
+    JPanel layer3 = new JPanel();      
+    layer3.add(buttons.get("4"));        
+    layer3.add(buttons.get("5"));
+    layer3.add(buttons.get("6"));
+    layer3.add(buttons.get("*"));
+    
+    JPanel layer4 = new JPanel();      
+    layer4.add(buttons.get("1"));        
+    layer4.add(buttons.get("2"));
+    layer4.add(buttons.get("3"));
+    layer4.add(buttons.get("-"));
         
-        JPanel layer4 = new JPanel();      
-        layer4.add(buttons.get("1"));        
-        layer4.add(buttons.get("2"));
-        layer4.add(buttons.get("3"));
-        layer4.add(buttons.get("-"));
-        
-        JPanel layer5 = new JPanel();      
-        layer5.add(buttons.get("0"));        
-        layer5.add(buttons.get("."));
-        layer5.add(buttons.get("(-)"));
-        layer5.add(buttons.get("+"));
+    JPanel layer5 = new JPanel();      
+    layer5.add(buttons.get("0"));        
+    layer5.add(buttons.get("."));
+    layer5.add(buttons.get("(-)"));
+    layer5.add(buttons.get("+"));
         
    
-        JPanel main = new JPanel();
-        main.setLayout(new GridLayout(8,1));
-        main.add(function);
-        main.add(feilds);
-        main.add(function);
-        main.add(layer1);
-        main.add(layer2);
-        main.add(layer3);
-        main.add(layer4);
-        main.add(layer5);
-        
-
-
-
-        
-        frame.add(main);        
-        frame.setSize(500, 700);
-        frame.show();
-        
+    JPanel main = new JPanel();
+    main.setLayout(new GridLayout(8,1));
+    main.add(function);
+    main.add(feilds);
+    main.add(function);
+    main.add(layer1);
+    main.add(layer2);
+    main.add(layer3);
+    main.add(layer4);
+    main.add(layer5);
+                
+    frame.add(main);        
+    frame.setSize(500, 700);
+    frame.show();
 	}
+
 	
 	public Calculator() {
 		System.out.println("Calculator Running");
@@ -179,7 +173,7 @@ public class Calculator extends JFrame implements ActionListener {
 		o.setText(otext.replace('~', '-'));
 	}
 	
-	private static String append(String s, char k) {
+	private String append(String s, char k) {
 		System.out.println(k);
 		
 		String nums = "~0123456789.r()";
@@ -228,7 +222,7 @@ public class Calculator extends JFrame implements ActionListener {
     	return s;
 	}
 	
-	private static void scrollHistory(Boolean up) {
+	private void scrollHistory(Boolean up) {
 		if (up) {
 			if (historyIndex > 0) {
 				historyIndex --;
@@ -242,7 +236,7 @@ public class Calculator extends JFrame implements ActionListener {
 		}
 	}
 	
-	private static double equals() {
+	private double equals() {
 		String ftext = otext.replace('~', '-');
 		double ans = Communicator.getNum(ftext);
 		String eq = ftext + " = " + doubleString(ans); 
@@ -257,5 +251,9 @@ public class Calculator extends JFrame implements ActionListener {
 		    return String.format("%s", d);
 		else
 		    return ("" + d).split(".0")[0];
+	}
+	
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
 	}
 }
